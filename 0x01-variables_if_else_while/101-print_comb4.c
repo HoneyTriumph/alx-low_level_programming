b3781fbc6ba5f3a7b413566c55e63af9fbd162ef
HoneyTriumph <triumphanthoney@gmail.com>
2022-09-12 18:04:51 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-
 /**
-*  * main - Entry point
-*   *
-*    * Return: Always 0 (Success)
-*/
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int i, i2, i3;
@@ -17,6 +16,7 @@ for (i = '0'; i < '9'; i++)
 for (i2 = i + 1; i2 <= '9'; i2++)
 {
 for (i3 = i2 + 1; i3 <= '9'; i3++)
+{
 putchar(i);
 putchar(i2);
 putchar(i3);
