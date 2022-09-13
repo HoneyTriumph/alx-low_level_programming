a0f6c12a207b6ef46b710f472a01423fbd987418
HoneyTriumph <triumphanthoney@gmail.com>
2022-09-12 18:20:52 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
-*  * main - the function main be generates a random number a validate
-*   *
-*    * Return: Always 0 (Sucess)
-*/
+ *  * main - the function main be generates a random number a validate
+ *   *
+ *    * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
 int n;
